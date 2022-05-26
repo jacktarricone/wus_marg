eee5b240f793965b56b391fa4e0abf340cf8aed8
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2022-05-26 14:46:32 -0400

    sierra tile

--- testing.R
+++ testing.R
@@ -4,12 +4,13 @@
 
 library(terra)
 library(ncdf4)
+require(XML)
 
 # set working directory
-setwd("/Users/jacktarricone/wus_marg/test_data/")
+setwd("/Users/jacktarricone/wus_marg/sierra_tile/")
 
 # file path for 2017 swe and sca ncdf4
-name <-"/Users/jacktarricone/wus_marg/test_data/WUS_UCLA_SR_v01_N37_0W120_0_agg_16_WY2017_18_SWE_SCA_POST.nc"
+name <-"/Users/jacktarricone/wus_marg/sierra_tile/WUS_UCLA_SR_v01_N39_0W121_0_agg_16_WY1993_94_SWE_SCA_POST.nc"
 ncin <- nc_open(name)
 dname <- "SWE_Post"  # define variable name
 print(ncin) # print netcdf contents
@@ -32,22 +33,32 @@ mean_swe_array <-swe_array[,,1,]
 # convert to raster
 mean_swe_rast <-rast(mean_swe_array)
 mean_swe_rast 
-plot(mean_swe_rast[[170]]) # test plot
+plot(mean_swe_rast[[200]]) # test plot
 
 ### geolocate and project
 # !!!!this is not the right way to do this given the variable pixel size
 
-# set extent (from xml file)
-# in the future, think about how to loop over the whole thing and do this
-# would need code to extract
-ext(mean_swe_rast)<- ext(-119.998,-119.001,37.0023,37.9978)
+# parse xml file
+data <- xmlParse("WUS_UCLA_SR_v01_N39_0W121_0_agg_16_WY1993_94_SWE_SCA_POST.nc.xml")
+xml_data <- xmlToList(data)
+print(xml_data)
+
+# bounding box for geolocation
+xmin <-as.numeric(xml_data[["GranuleURMetaData"]][["SpatialDomainContainer"]][["HorizontalSpatialDomainContainer"]][["BoundingRectangle"]][["WestBoundingCoordinate"]])
+xmax <-as.numeric(xml_data[["GranuleURMetaData"]][["SpatialDomainContainer"]][["HorizontalSpatialDomainContainer"]][["BoundingRectangle"]][["EastBoundingCoordinate"]])
+ymin <-as.numeric(xml_data[["GranuleURMetaData"]][["SpatialDomainContainer"]][["HorizontalSpatialDomainContainer"]][["BoundingRectangle"]][["SouthBoundingCoordinate"]])
+ymax <-as.numeric(xml_data[["GranuleURMetaData"]][["SpatialDomainContainer"]][["HorizontalSpatialDomainContainer"]][["BoundingRectangle"]][["NorthBoundingCoordinate"]])
+
+
+# set extent
+ext(mean_swe_rast)<- ext(xmin,xmax,ymin,ymax)
 
 # set crs
 crs(mean_swe_rast) <-"epsg:4326"
 
 # test plot
-plot(mean_swe_rast[[170]])
-writeRaster(mean_swe_rast[[220]], "test2.tif")
+plot(mean_swe_rast[[100]])
+writeRaster(mean_swe_rast[[210]], "test6.tif")
 
 
 
