932db36b34b9a03cfe2b36bfe17e2eeefe6d4d28
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2022-09-25 12:31:07 -0700

    updates

--- gif_making_test.R
+++ gif_making_test.R
@@ -4,20 +4,22 @@
 library(terra)
 library(lubridate)
 library(ncdf4)
+library(ggmap)
 library(gganimate)
 library(gapminder)
 library(ggplot2);theme_set(theme_classic(12))
-
+library(RColorBrewer)
 
 # set working directory for full year i just got off nsidc
-setwd("/Volumes/jack_t/wus_marg/wy1984")
+setwd("/Users/jacktarricone/wus_marg/sierra_tile")
 
 # list all netcdf files for 1984 wy
 nc_list <-list.files(pattern = "SWE_SCA_POST.nc$", full.names = TRUE)
+nc_list
 
 # pull out one near tahoe
-tahoe <-nc_list[163]
-tahoe
+wy2018 <-nc_list[35]
+wy2018
 
 ###### swe data info ######
 # gives us 4 dimensions here (nrow,ncol,statitics,time)
@@ -29,50 +31,76 @@ tahoe
 # stat_5 = 75th
 
 # bring swe variable into array
-ncin <- nc_open(tahoe) # open tahoe tile
+ncin <- nc_open(wy2018) # open tahoe tile
 swe_array <- ncvar_get(ncin,"SWE_Post") # read in
 med_swe_array <-swe_array[,,3,] # pull out med SWE
 
 # convert to raster stack and project
 stack <-rast(med_swe_array)
-ext(stack)<- ext(-121,-120,38,39)
+ext(stack)<- ext(-121,-120,39,40)
 crs(stack) <-"epsg:4326"
 stack # inspect
-plot(stack[[170]])
+plot(stack[[230]])
 
 # convert to df for ggplot
 stack_df <-as.data.frame(stack, xy = TRUE)
 
 # create dates sequence
-dates_seq <-seq(ymd(origin = "1984-10-01"), ymd("1985-09-30"),1)
+dates_seq <-seq(ymd(origin = "2017-10-01"), ymd("2018-09-30"),1)
 colnames(stack_df)[3:367] <-as.character(dates_seq) # rename cols to dowy numbers
+head(stack_df)
+stack_df[stack_df == 0] <-NA
 
 # pivot for plotting
 stack_df_plot <-tidyr::pivot_longer(stack_df,3:367) # pivot
 colnames(stack_df_plot)[3:4] <-c("date","swe") # rename
 stack_df_plot$date <-ymd(stack_df_plot$date) # convert to date
-test <-as.data.frame(
-  dplyr::filter(stack_df_plot, date >= ymd("1985-03-25") & date <= ymd("1985-04-10")))
+class(stack_df_plot)
 
-# plot
+# change 0 to nan for plotting
+stack_df_plot[stack_df_plot == 0] <-NA
+
+# date filtering
+# <-as.data.frame(dplyr::filter(stack_df_plot, date >= ymd("1985-03-25") & date <= ymd("1985-04-10")))
+
+# set map bounding box
+loc <-c(-121,39,-120,40)
+
+# download google sat
+myMap <- get_map(location=loc,
+                 source="google", maptype="satellite", crop=TRUE)
+
+# see map, looks good
+ggmap(myMap) 
+
+# set scale
+blues_scale <-brewer.pal(9, 'Blues')
+
+# color test
+ggmap(myMap) +
+  geom_raster(stack_df, mapping = aes(x,y, fill = `2018-04-01`)) +
+  labs(x="Lon (deg)",y="Lat (deg)")+
+  coord_equal()+
+  scale_fill_gradientn(colours = blues_scale, limits = c(0,2.5), na.value="transparent")
+  #theme(strip.background = element_rect(colour="white", fill="white")) 
+  #transition_time(date) +
+  #labs(title = "North Lake Tahoe SWE: {frame_time}")
+  #ease_aes("linear")
+
+# plot for animation
 plot <-ggplot(stack_df_plot,
               aes(x,y, fill = swe, group = date)) +
-  geom_raster() +
+  geom_raster() + 
   labs(x="Lon (deg)",y="Lat (deg)")+
-  coord_equal() +
-  labs(fill = "SWE (m)") + #, title = "Grand Mesa Unwrapped Phase 2021 UAVSAR Time Series") +
-  scale_fill_gradientn('UNW (rad)', limits = c(0,1.5),
-                       colours = colorRampPalette(c("white", "darkblue"))(20)) +
-  theme(strip.background = element_rect(colour="white", fill="white")) +
-  transition_time(date) +
+  coord_equal() + # set aspect ratio
+  scale_fill_gradientn(colours = blues_scale, limits = c(0,3), na.value = 'darkblue') + # colors
+  theme(strip.background = element_rect(colour="white", fill="white")) + # format
+  transition_time(date) + # format gif by date
   labs(title = "North Lake Tahoe SWE: {frame_time}")+
-  ease_aes("linear")
-
-#check
-plot
+  ease_aes("linear") # ow gifs change
 
 # render and save
 setwd("/Users/jacktarricone/wus_marg/")
-anim_save("north_lake_swe_map_1984.gif",plot)
-
+animate(plot, duration = 30)
+anim_save("north_lake_swe_map_2018.gif", animation = last_animation())
 
